ea7d7b2223084b1d4979b6ed6de03b94657d297b
Onur Demir <onur.demir@mdpgroup.com>
2023-08-14 15:19:01 +0300

    Basket microservices have been established. Redis configuration applied. Redis db created as a container in docker environment.

---
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        private IHttpContextAccessor httpContextAccessor;
+
+        public SharedIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId => httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
