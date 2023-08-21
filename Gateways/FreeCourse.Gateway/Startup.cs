1b3f7c3f5b29b129253fff755ad7e0225d7462f9
Onur Demir <onur.demir@mdpgroup.com>
2023-08-21 17:28:08 +0300

    Authentication was provided for gateway.

--- Gateways/FreeCourse.Gateway/Startup.cs
+++ Gateways/FreeCourse.Gateway/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
@@ -14,10 +16,23 @@ namespace FreeCourse.Gateway
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_gateway";
+                options.RequireHttpsMetadata = false;
+            });
             services.AddOcelot();
         }
 
