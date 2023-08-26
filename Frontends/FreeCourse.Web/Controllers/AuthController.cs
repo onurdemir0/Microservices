9ead29fe803a17fe15e25d11fa24baf430f4964c
Onur Demir <onur.demir@mdpgroup.com>
2023-08-26 18:11:37 +0300

    Logout action has written.

--- Frontends/FreeCourse.Web/Controllers/AuthController.cs
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Abstract;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -36,9 +38,14 @@ namespace FreeCourse.Web.Controllers
                 return View();
             }
 
-
-
             return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
