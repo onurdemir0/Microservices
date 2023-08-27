d30e5cd0f27fbb8d0b168e6285e09d8c0dc0174e
Onur Demir <onur.demir@mdpgroup.com>
2023-08-27 17:43:00 +0300

    ICatalog Service has been created.

---
+++ Frontends/FreeCourse.Web/Services/Abstract/ICatalogService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Web.Models.Catalog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Abstract
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourse();
+        Task<List<CourseViewModel>> GetAllCourseByUserId(string userId);
+        Task<CourseViewModel> GetByCourseId(string courseId);
+
+        Task<bool> CreateCourse(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourse(CourseUpdateInput courseUpdateInput);
+        Task<bool> DeleteCourse(string courseId);
+    }
+}
