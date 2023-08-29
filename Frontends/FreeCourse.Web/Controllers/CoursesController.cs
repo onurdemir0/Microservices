6492f3f19efc4279d39ad7bb8360021f89f6bdfc
Onur Demir <onur.demir@mdpgroup.com>
2023-08-29 15:29:23 +0300

    Course controller and index page were written.

---
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -0,0 +1,26 @@
+﻿using FreeCourse.Shared.Services;
+using FreeCourse.Web.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private ISharedIdentityService _sharedIdentityService;
+
+        public CoursesController(ICatalogService catalogService, ISharedIdentityService sharedIdentityService)
+        {
+            _catalogService = catalogService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _catalogService.GetAllCourseByUserId(_sharedIdentityService.GetUserId));
+        }
+    }
+}
