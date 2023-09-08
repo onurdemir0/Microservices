ba0fbba46b2d3aa378a552a91c7c69b40fe9e5d6
Onur Demir <onur.demir@mdpgroup.com>
2023-09-08 13:49:58 +0300

    Photo stock upload and delete methods were written.

--- Frontends/FreeCourse.Web/Services/Concrete/PhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/Concrete/PhotoStockService.cs
@@ -1,20 +1,49 @@
 ﻿using FreeCourse.Web.Models.PhotoStocks;
 using FreeCourse.Web.Services.Abstract;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services.Concrete
 {
     public class PhotoStockService : IPhotoStockService
     {
-        public Task<bool> DeletePhoto(string photoUrl)
+        private readonly HttpClient _httpClient;
+
+        public PhotoStockService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
         }
 
-        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        public async Task<bool> DeletePhoto(string photoUrl)
         {
-            throw new System.NotImplementedException();
+            var response = await _httpClient.DeleteAsync($"photos?photoUrl={photoUrl}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        {
+            if (photo == null || photo.Length <= 0)
+                return null;
+
+            //-- 123235023952.jpg
+            var randomFileName = $"{Guid.NewGuid().ToString()}.{Path.GetExtension(photo.FileName)}";
+
+            using var ms = new MemoryStream();
+
+            await photo.CopyToAsync(ms);
+
+            var multipartContent = new MultipartFormDataContent();
+            multipartContent.Add(new ByteArrayContent(ms.ToArray()), "photo", randomFileName);
+
+            var response = await _httpClient.PostAsync("photos", multipartContent);
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadFromJsonAsync<PhotoViewModel>();
         }
     }
 }
