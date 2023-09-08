c4d5addce2c675c6e0a0ff3863223c7b0435738e
Onur Demir <onur.demir@mdpgroup.com>
2023-09-08 13:27:57 +0300

    Photo stock interface and service implementation.

---
+++ Frontends/FreeCourse.Web/Services/Concrete/PhotoStockService.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Web.Models.PhotoStocks;
+using FreeCourse.Web.Services.Abstract;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Concrete
+{
+    public class PhotoStockService : IPhotoStockService
+    {
+        public Task<bool> DeletePhoto(string photoUrl)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
