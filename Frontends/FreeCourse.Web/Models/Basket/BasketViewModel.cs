d19f4399656f81385ad1f9ba9c3bea7dfcb95bc1
Onur Demir <onur.demir@mdpgroup.com>
2023-09-17 23:27:36 +0300

    Basket view models have created.

---
+++ Frontends/FreeCourse.Web/Models/Basket/BasketViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Web.Models.Basket
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        private List<BasketItemViewModel> _basketItems { get; set; }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (HasDiscount)
+                {
+                    _basketItems.ForEach(b =>
+                    {
+                        var discountPrice = b.Price * ((decimal)DiscountRate.Value / 100);
+                        b.AppliedDiscount(Math.Round(b.Price - discountPrice, 2));
+                    });
+                }
+                return _basketItems;
+            }
+            set { _basketItems = value; }
+        }
+        public decimal TotalPrice
+        {
+            get => _basketItems.Sum(x => x.GetCurrentPrice);
+        }
+
+        public bool HasDiscount
+        {
+            get => !string.IsNullOrEmpty(DiscountCode);
+        }
+    }
+}
