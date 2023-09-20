49ee4565486b9d1c671cb688df1091f932ab45af
Onur Demir <onur.demir@mdpgroup.com>
2023-09-20 17:31:21 +0300

    Basket controller has created.

---
+++ Frontends/FreeCourse.Web/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using FreeCourse.Web.Models.Basket;
+using FreeCourse.Web.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Get());
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseId(courseId);
+            var basketItemViewModel = new BasketItemViewModel { CourseId = course.Id, CourseName = course.Name, Price = course.Price };
+
+            await _basketService.AddBasketItem(basketItemViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId); 
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
