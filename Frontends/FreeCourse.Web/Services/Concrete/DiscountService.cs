5cd1b6d30576e196e9d6d8ff5bc03dbb1ab26f4e
Onur Demir <onur.demir@mdpgroup.com>
2023-09-22 16:02:10 +0300

    Discount services were created. Http client has written into extension class.

---
+++ Frontends/FreeCourse.Web/Services/Concrete/DiscountService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Web.Models.Discount;
+using FreeCourse.Web.Services.Abstract;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Concrete
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
