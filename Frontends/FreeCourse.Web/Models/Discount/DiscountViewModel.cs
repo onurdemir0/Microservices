517ba7486cb11f73a8f753efa11d7aa127e25212
Onur Demir <onur.demir@mdpgroup.com>
2023-09-22 15:55:34 +0300

    Discount view model and fluent validation for discount input model have created.

---
+++ Frontends/FreeCourse.Web/Models/Discount/DiscountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FreeCourse.Web.Models.Discount
+{
+    public class DiscountViewModel
+    {
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+    }
+}
