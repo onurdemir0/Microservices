02edc430a58746e44d9ac63253defa275b66a5bb
Onur Demir <onur.demir@mdpgroup.com>
2023-09-30 20:40:45 +0300

    Checkout model has created for order.

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name ="İl")]
+        public string Province { get; private set; }
+
+        [Display(Name = "İlçe")]
+        public string District { get; private set; }
+
+        [Display(Name = "Cadde")]
+        public string Street { get; private set; }
+
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; private set; }
+
+        [Display(Name = "Adres")]
+        public string Line { get; private set; }
+
+        [Display(Name = "Kart isim soyisim")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Kart no")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Son kullanma tarihi(ay/yıl)")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV/CVC2 numarası")]
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
