0c64590f4098a1da0f2445b68826be6350ef7aa2
Onur Demir <onur.demir@mdpgroup.com>
2023-10-02 10:54:19 +0300

    Create order and get order methods were written.

---
+++ Frontends/FreeCourse.Web/Models/Orders/OrderCreateInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class OrderCreateInput
+    {
+        public OrderCreateInput()
+        {
+            OrderItems = new List<OrderItemViewModel>();
+        }
+        public string BuyerId { get; set; }
+        public List<OrderItemViewModel> OrderItems { get; set; }
+        public AddressCreateInput Address { get; set; }
+    }
+}
